cce60593176dbb85a47ea7b13ab2cd8b14980710
Klounada2025hack <dark_0n@mail.ru>
2026-05-30 19:09:11 +0300

    auth

--- frontend/src/pages/Authorization.jsx
+++ frontend/src/pages/Authorization.jsx
@@ -4,24 +4,37 @@ import Button from "../components/Button/button";
 import Box from "../components/Box/Box";
 import Smallbox from "../components/Box/smallbox";
 import Button_reRoll from "../components/Button_reRoll/Button_reRoll";
+import { useNavigate } from "react-router-dom";
 
-function Authorization(){
+export default function Authorization(){
     const [Name,setName] = useState("");
+    const navigate = useNavigate();
+    const handleLogin = () => {
+        if(!Name.trim()) 
+            
+            return 
+        alert("йоу имя то у тебя есть? Иначе сам придумаю!") 
+        navigate("/home")
+    }
 
     return(
 
-<div className="layout">
-    <div>
-        <Button_reRoll
+    <div className="layout">
+        <div className="Container">
+            <Button_reRoll
                 variant="input"
                 value={Name}
                 placeholder="Введите Свое имя"
                 onChange={(e) => setName(e.target.value)}
             />
+            <Button
+                variant="small"
+                text="Продолжить"
+                onClick={handleLogin}
+            />
+        </div>
     </div>
-</div>
-);
+  );
 }
 
-export default Authorization
 
